cfba4dd2416d727b09812c38d5c958df94160135
KhalidNC <hussainkhalid70@yahoo.com>
2023-11-15 17:21:40 +0600

    modified intro.py and gitignore

--- basic/intro.py
+++ basic/intro.py
@@ -1,15 +1,19 @@
 print("Hello World!")
 
+
 # Create a function that multiple 2 numbers
 def multiple(a, b):
-  return a * b
+    return a * b
+
 
 results = multiple(5, 6)
 print(results)
 
+
 # Function that adds 2 numbers
 def sum(a, b):
-  return a + b
+    return a + b
+
 
 results1 = sum(3, 9)
 print(results1)
@@ -18,4 +22,4 @@ print(results1)
 cars = ["Toyota", "Nissan", "BMW", "Audi", "Tesla"]
 
 for i in cars:
-  print(i)
+    print(i)
